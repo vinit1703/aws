57808184a2821e88d83f74f6e3c008a3edb11774
vinit <vinitmay17@gmail.com>
2024-07-25 13:13:23 +0530

    changed message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vinit learn AWS amplify with deploying React
         </a>
       </header>
     </div>
